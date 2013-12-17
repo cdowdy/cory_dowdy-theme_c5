6f3f2d99e8aa8af75591d18182965d8fb7a6b907
cdowdy <cdowdy46@gmail.com>
2013-12-17 14:58:17 -0500

    new default php

--- themes/cory_dowdy/default.php
+++ themes/cory_dowdy/default.php
@@ -1,5 +1,40 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied."));
 $this->inc('elements/header.php');
 ?>
+<div class="page-wrap">
+  <section class="top-color1">
+    <div class="row">
+      <div class="medium-9 medium-centered columns">
+        <h1>I'm Cory,</h1>
+        <h4>I do "web things". I love working with small businesses and responsive web development.</h4>
+      </div>
+    </div>
+  </section>
+
+  <section class="middle">
+    <div class="row">
+      <a href="#">
+        <div class="small-12 medium-4 columns s-m-bg">
+          <div class="featured computer-icon-work"></div>
+          <h4>Work</h4>
+        </div>
+      </a>
+      <a href="#">
+        <div class="small-12 medium-4 columns s-m-bg">
+          <div class="featured computer-icon-blog"></div>
+          <h4>Blog</h4>
+        </div>
+      </a>
+      <a href="#">
+        <div class="small-12 medium-4 columns s-m-bg">
+          <div class="featured computer-icon-demo"></div>
+          <h4>Demos</h4>
+        </div>
+      </a>
+    </div>
+  </section>
+
+</div><!-- page wrap -->
+
 
 <?php $this->inc('elements/footer.php'); ?> 
