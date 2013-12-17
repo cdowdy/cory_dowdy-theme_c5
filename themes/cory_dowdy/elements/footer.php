018bd250a0a2abedd92b5035f774631973c3af55
cdowdy <cdowdy46@gmail.com>
2013-12-17 14:58:23 -0500

    new footer

--- themes/cory_dowdy/elements/footer.php
+++ themes/cory_dowdy/elements/footer.php
@@ -1,41 +1,34 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
 <!-- Footer -->
- 
-      <footer class="row">
-        <div class="large-12 columns"><hr />
-          <div class="row">
- 
-            <div class="large-6 columns">
-              <p>© Copyright no one at all. Go to town.</p>
-            </div>
- 
-            <div class="large-6 columns">
-              <ul class="inline-list right">
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li><a href="#">Link 4</a></li>
-              </ul>
-            </div>
- 
-          </div>
+<footer class="f-primary">
+  <div class="row">
+    <div class="small-12 columns">
+      <hr />
+      <div class="row">
+        <div class="small-12 large-6 columns">
+         <p class="copyright">&copy; <?php print date('Y') . ' ' . SITE; ?></p>
         </div>
-      </footer>
- 
-    <!-- End Footer -->
- 
-    </div>
+        <div class="small-12 large-6 columns">
+          <ul class="footer-links">
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Demos</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div> 
   </div>
-    
-    
-    <?php if (!$c->isEditMode()) { ?>
-    <script src="<?php echo $this->getThemePath(); ?>/js/vendor/min/jquery.min.js"></script>
-    <script src="<?php echo $this->getThemePath(); ?>/js/foundation/min/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
-    <?php } ?>
-    <?php Loader::element('footer_required'); ?>
+</footer>
+
+<?php $c = Page::getCurrentPage();     
+if (!$c->isEditMode()) { ?>
+<script src="<?php echo $this->getThemePath(); ?>/min/CoryDowdy.min.js"></script>
+<script>
+  $(document).foundation();
+</script>
+<?php } ?>
+<?php Loader::element('footer_required'); ?>
   </body>
 </html>
