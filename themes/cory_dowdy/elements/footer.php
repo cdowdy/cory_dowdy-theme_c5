61953d1717642b9a707ec8b973ef2694c7665e9b
cdowdy <cdowdy46@gmail.com>
2013-12-26 16:23:52 -0500

    demo js added to footer

--- themes/cory_dowdy/elements/footer.php
+++ themes/cory_dowdy/elements/footer.php
@@ -34,6 +34,16 @@ $(document).ready(function(){$(".image-popup-vertical-fit").magnificPopup({type:
 });
 </script>
 <?php endif; ?>
+<?php if ($c->getCollectionHandle() == 'demos'):?>
+<script>
+$(document).ready(function(){$(".image-popup-vertical-fit").magnificPopup({type:"image",closeOnContentClick:true,mainClass:"mfp-img-mobile",image:{verticalFit:true}});
+$(".image-popup-fit-width").magnificPopup({type:"image",closeOnContentClick:true,image:{verticalFit:false}});$(".image-popup-no-margins").magnificPopup({type:"image",closeOnContentClick:true,closeBtnInside:false,fixedContentPos:true,mainClass:"mfp-no-margins mfp-with-zoom",image:{verticalFit:true},zoom:{enabled:true,duration:300}});
+$(".popup-gallery").magnificPopup({delegate:"a",type:"image",tLoading:"Loading image #%curr%...",mainClass:"mfp-img-mobile",gallery:{enabled:true,navigateByImgClick:true,preload:[0,1]},image:{tError:'<a href="%url%">The image #%curr%</a> could not be loaded.',titleSrc:function(a){return a.el.attr("title")+"<small>Images from Placehold.it";
+}}});$(".zoom-gallery").magnificPopup({delegate:"a",type:"image",closeOnContentClick:false,closeBtnInside:false,mainClass:"mfp-with-zoom mfp-img-mobile",image:{verticalFit:true,titleSrc:function(a){return a.el.attr("title")+' &middot; <a class="image-source-link" href="'+a.el.attr("data-source")+'" target="_blank">image source</a>';
+}},gallery:{enabled:true},zoom:{enabled:true,duration:300,opener:function(a){return a.find("img");}}});$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({disableOn:700,type:"iframe",mainClass:"mfp-fade",removalDelay:160,preloader:false,fixedContentPos:false});
+$("img").VimeoThumb();});
+</script>
+<?php endif;?>
 <?php Loader::element('footer_required'); ?>
   </body>
 </html>
